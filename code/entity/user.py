2eb28eae755476bf7629a95a2f20ac4d4d4443d8
corbinhuang <corbinhuang@tencent.com>
2022-07-06 16:37:03 +0800

    feat: 🎸 添加用户、角色实体类

---
+++ code/entity/user.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from __future__ import annotations
+
+
+class User:
+    """
+    用户实体类
+    """
+
+    def __init__(self, id: str, name: str) -> None:
+        self.__id = id
+        self.__name = name
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id: str):
+        if isinstance(id, str):
+            self.__id = id
+        else:
+            raise TypeError('user_id不是字符串类型')
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError('user_name不是字符串类型')
